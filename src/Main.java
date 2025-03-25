0d7e5c65686845afdd7876ada75b6886d43843e9
GrigoroiuAlexandru <gheorghealexandru.grigoroiu@ulbsibiu.ro>
2025-03-25 15:29:06 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,63 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.*;
+import java.util.*;
+
+class Student {
+    String nume;
+    String grupa;
+    List<Integer> note;
+
+    public Student(String nume, String grupa, List<Integer> note) {
+        this.nume = nume;
+        this.grupa = grupa;
+        this.note = note;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(nume, student.nume) &&
+                Objects.equals(grupa, student.grupa) &&
+                Objects.equals(note, student.note);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nume, grupa, note);
+    }
+
+    @Override
+    public String toString() {
+        return nume + " " + grupa + " " + note;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Map<Student, Integer> studentMap = new HashMap<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(" ");
+                String nume = parts[0] + " " + parts[1];
+                String grupa = parts[2];
+                List<Integer> note = new ArrayList<>();
+                for (int i = 3; i < parts.length; i++) {
+                    note.add(Integer.parseInt(parts[i]));
+                }
+
+                Student student = new Student(nume, grupa, note);
+                studentMap.put(student, studentMap.getOrDefault(student, 0) + 1);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Studentii si numarul de aparitii:");
+        for (Map.Entry<Student, Integer> entry : studentMap.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
         }
     }
 }
